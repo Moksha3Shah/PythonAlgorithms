29fcbcddacf489ddcb44a0205113aec062b69262
Moksha Shah <58504326+Moksha3Shah@users.noreply.github.com>
2020-10-16 10:51:05 +0530

    Update 237_Delete_Node_in_a_Linked_List.py

--- LeetCode/237_Delete_Node_in_a_Linked_List.py
+++ LeetCode/237_Delete_Node_in_a_Linked_List.py
@@ -5,10 +5,42 @@
 #         self.next = None
 
 class Solution:
-    def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        node.next = node.next.next
+
+def __init__(self,value,next=None):
+    self.value = value
+    self.next = next
+
+def getNext(self):
+    return self.next
+
+def getValue(self):
+    return self.value
+
+class LinkedList:
+
+def __init__(self,node):
+    self.node = node
+
+def __str__(self):
+    node = self.node
+    s = "[Node " + str(node.getValue())
+    if node.getNext() is None:
+        return s + "]"
+    while node.next is not None:
+        s += ", Node " + str(node.next.value)
+        node = node.next
+    return s + "]"
+
+def delete(self,value):
+    head = self.node
+    if head.getValue() == value:
+        return LinkedList(head.next)
+    temp = head
+    while temp.next is not None:
+        if temp.next.getValue() == value:
+            temp = temp.next.next
+                return self
+            temp.next = None
+            return LinkedList(head)
+        temp = temp.next
+    return "Sorry node is not here!"
